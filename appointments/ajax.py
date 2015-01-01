1ceb0b7de21b42863bb0749e27a87a8e25c808d4
moshthepitt <kelvin@jayanoris.com>
2015-01-02 02:44:33 +0300

    removed bootstrap-calendar

--- appointments/ajax.py
+++ appointments/ajax.py
@@ -30,28 +30,3 @@ def event_feed(request):
             "error": "Something went wrong"
         }
         return HttpResponse(json.dumps(data), content_type="application/json")
-
-# def event_feed(request):
-#     if request.method == 'GET':
-#         if 'from' in request.GET and request.GET['from'].isdigit() and 'to' in request.GET and request.GET['to'].isdigit():
-#             fro = timezone.make_aware(datetime.datetime.fromtimestamp(int(request.GET['from'])/1000),timezone.get_current_timezone())
-#             to = timezone.make_aware(datetime.datetime.fromtimestamp(int(request.GET['to'])/1000),timezone.get_current_timezone())
-#             period = Period(Event.objects.all(),fro,to)
-#             occurences = [{
-#                             'title':x.title,
-#                             'class':'event-info',
-#                             'start':calendar.timegm(x.start.timetuple())*1000,
-#                             'end':calendar.timegm(x.end.timetuple())*1000
-#                           }
-#                           for x in period.get_occurrences()]
-#         data = {
-#             "success": 1,
-#             "result": occurences
-#         }
-#         return HttpResponse(json.dumps(data), content_type="application/json")
-#     else:
-#         data = {
-#             "success": 0,
-#             "error": "Something went wrong"
-#         }
-#         return HttpResponse(json.dumps(data), content_type="application/json")
