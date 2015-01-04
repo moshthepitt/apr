842f217c28a5cf5b3de0e488f5b01fd2195f0516
moshthepitt <kelvin@jayanoris.com>
2015-01-04 20:54:55 +0300

    added documentation folder

---
+++ documentation/dev/django/dev.local_settings.py
@@ -0,0 +1,41 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+#Emails
+#Email
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+DEFAULT_FROM_EMAIL='Jaya <support@jayanoris.com>'
+
+
+#static
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+STATIC_ROOT = '/static/'
+
+#CACHE
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+#DEBUG
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
