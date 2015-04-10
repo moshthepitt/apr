660e41dc6a67447ae1a6fb80a7d8123c03a7f6b9
moshthepitt <kelvin@jayanoris.com>
2015-04-10 23:59:16 +0300

    added venue creator

--- venues/models.py
+++ venues/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
 
 from core import labels
 
@@ -8,6 +9,7 @@ class Venue(models.Model):
     created_on = models.DateTimeField(_("created on"), auto_now_add=True)
     updated_on = models.DateTimeField(_("updated on"), auto_now=True)
     name = models.CharField(_('Venue name'), max_length=255, blank=True)
+    creator = models.ForeignKey(User, verbose_name=_("Creator"), on_delete=models.PROTECT)
 
     class Meta:
         verbose_name = getattr(labels, 'VENUE', _("Venue"))
