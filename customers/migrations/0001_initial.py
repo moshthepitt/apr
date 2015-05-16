03d077339b9c780caf968f6b1d249aa10c40e9b6
moshthepitt <kelvin@jayanoris.com>
2015-05-17 00:30:57 +0300

    added customers model + migrations

---
+++ customers/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+from django.conf import settings
+import phonenumber_field.modelfields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Customer',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created_on', models.DateTimeField(auto_now_add=True, verbose_name='created on')),
+                ('updated_on', models.DateTimeField(auto_now=True, verbose_name='updated on')),
+                ('name', models.CharField(max_length=255, verbose_name='Customer name')),
+                ('email', models.EmailField(max_length=254, verbose_name='Email address', blank=True)),
+                ('phone', phonenumber_field.modelfields.PhoneNumberField(max_length=255, verbose_name='Phone Number', blank=True)),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this assistant should be treated as active.', verbose_name='Active')),
+                ('creator', models.ForeignKey(related_name='customer_creator', on_delete=django.db.models.deletion.PROTECT, verbose_name='Creator', to=settings.AUTH_USER_MODEL)),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, default=None, to=settings.AUTH_USER_MODEL, blank=True, help_text='This user will be able to log in as this customer', null=True, verbose_name='User')),
+            ],
+            options={
+                'ordering': ['name'],
+                'verbose_name': 'Customer',
+                'verbose_name_plural': 'Customers',
+            },
+        ),
+    ]
--- customers/models.py
+++ customers/models.py
@@ -10,13 +10,15 @@ class Customer(models.Model):
     """
     This model stores customers i.e. peopl who sign up to user APR
     """
+    user = models.ForeignKey(User, verbose_name=_("User"), null=True, default=None, blank=True,
+                             on_delete=models.PROTECT, help_text=_("This user will be able to log in as this customer"))
     created_on = models.DateTimeField(_("created on"), auto_now_add=True)
     updated_on = models.DateTimeField(_("updated on"), auto_now=True)
     name = models.CharField(_('Customer name'), max_length=255, blank=False)
     email = models.EmailField(_('Email address'), blank=True)
     phone = PhoneNumberField(_('Phone Number'), max_length=255, blank=True)
     creator = models.ForeignKey(User, verbose_name=_(
-        "Creator"), on_delete=models.PROTECT, related_name="assistant_creator")
+        "Creator"), on_delete=models.PROTECT, related_name="customer_creator")
     is_active = models.BooleanField(_('Active'), default=True,
                                     help_text=_('Designates whether this assistant should be treated as '
                                                 'active.'))
