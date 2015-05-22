717ce45144a3b2784efffa5ef86f488a13980a90
moshthepitt <kelvin@jayanoris.com>
2015-05-22 13:02:23 +0300

    django admin for customers

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from customers.models import Customer
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Customer, CustomerAdmin)
