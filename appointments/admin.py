c9d973f9d78c4eb233dd885faa29913865163541
moshthepitt <kelvin@jayanoris.com>
2015-06-03 18:46:22 +0300

    addd html emails

--- appointments/admin.py
+++ appointments/admin.py
@@ -4,7 +4,7 @@ from appointments.models import Appointment
 
 
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'customer', 'client', 'doctor', 'venue', 'event']
-    list_filter = ['customer']
+    list_display = ['id', 'customer', 'client', 'doctor', 'venue', 'event', 'status']
+    list_filter = ['customer', 'client', 'status']
 
 admin.site.register(Appointment, AppointmentAdmin)
--- appointments/emails.py
+++ appointments/emails.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMultiAlternatives
 from django.template import Context
 from django.template.loader import render_to_string
 from django.contrib.sites.models import Site
@@ -21,21 +21,23 @@ def send_email_reminder(appointment):
 
     email_subject = render_to_string(
         'appointments/email/reminder_email_subject.txt', c).replace('\n', '')
-    email_body = render_to_string('appointments/email/reminder_email_body.txt', c)
+    email_txt_body = render_to_string('appointments/email/reminder_email_body.txt', c)
+    email_html_body = render_to_string('appointments/email/reminder_email_body.html', c)
 
     email_headers = {
         "X-Mailgun-Campaign-Id": "ffz23",
     }
 
-    email = EmailMessage(
+    email = EmailMultiAlternatives(
         email_subject,  # subject
-        email_body,  # body
+        email_txt_body,  # body
         settings.REMINDER_FROM_EMAIL,  # from
         [client_email],  # to
         # ['bcc@example.com'],  # bcc
         reply_to=[settings.REMINDER_FROM_EMAIL],
         headers=email_headers
     )
+    email.attach_alternative(email_html_body, "text/html")
 
     return email.send(fail_silently=False)
 
@@ -56,20 +58,22 @@ def send_cancel_email(appointment):
 
     email_subject = render_to_string(
         'appointments/email/cancel_notification_subject.txt', c).replace('\n', '')
-    email_body = render_to_string('appointments/email/cancel_notification_body.txt', c)
+    email_txt_body = render_to_string('appointments/email/cancel_notification_body.txt', c)
+    email_html_body = render_to_string('appointments/email/cancel_notification_body.html', c)
 
     email_headers = {
         "X-Mailgun-Campaign-Id": "fg0ec",
     }
 
-    email = EmailMessage(
+    email = EmailMultiAlternatives(
         email_subject,  # subject
-        email_body,  # body
+        email_txt_body,  # body
         settings.REMINDER_FROM_EMAIL,  # from
         [customer_email],  # to
         # ['bcc@example.com'],  # bcc
         reply_to=[settings.REMINDER_FROM_EMAIL],
         headers=email_headers
     )
+    email.attach_alternative(email_html_body, "text/html")
 
     return email.send(fail_silently=False)
