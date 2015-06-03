68df79447b822a5f9e6479b1227a79308d75ba6a
moshthepitt <kelvin@jayanoris.com>
2015-06-03 15:29:33 +0300

    pre_save to catch events whose timing changes significantly

--- appointments/signals.py
+++ appointments/signals.py
@@ -1,5 +1,9 @@
-from django.db.models.signals import post_delete
+from datetime import timedelta
+
+from django.db.models.signals import post_delete, pre_save
 from django.dispatch import receiver
+
+from schedule.models import Event
 
 from appointments.models import Appointment
 
@@ -12,3 +16,27 @@ def event_delete(sender, instance, **kwargs):
     if instance.event:
         this_event = instance.event
         this_event.delete()
+
+
+@receiver(pre_save, sender=Event)
+def event_changed(sender, instance, **kwargs):
+    """
+    if the event has been updated, we want to do a few things
+        1. if the new start time is more than half a day greater than original
+        then allow reminders to be sent (if they were turned off)
+    """
+    try:
+        obj = sender.objects.get(pk=instance.pk)
+    except sender.DoesNotExist:
+        # Object is new, so field hasn't technically changed, but you may want to
+        # do something else here.
+        pass
+    else:
+        if not obj.start == instance.start:  # Field has changed
+            if instance.start > obj.start:
+                diff = instance.start - obj.start
+                if diff > timedelta(hours=12):
+                    appointment = obj.appointment_set.first()
+                    if appointment:
+                        appointment.no_reminders = False
+                        appointment.save()
