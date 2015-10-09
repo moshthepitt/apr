48e91b78f9d1c900918ea3aa88426438c378bfcf
moshthepitt <kelvin@jayanoris.com>
2015-10-09 10:07:00 +0300

    method to clean duplicate clients

--- users/utils.py
+++ users/utils.py
@@ -1,5 +1,7 @@
 from django.utils import timezone
+from django.db.models import Count
 
+from appointments.models import Appointment
 from users.models import Client
 from users.sms import send_sms_birthday_greeting
 from users.emails import send_email_birthday_greeting
@@ -18,3 +20,21 @@ def send_birthday_greetings(today=timezone.now()):
                 if client.customer.birthday_greeting_send_sms:
                     send_sms_birthday_greeting(client)
     return
+
+
+def clean_clients():
+    """
+    Try to get rid of duplicate clients by making sure only one clients with uniqu client IDs exist
+    {'pk__count': 103, 'client_id': u''}
+    """
+    values = Client.objects.values('client_id').annotate(Count('pk')).filter(pk__count__gt=1)
+    for val in values:
+        if val['client_id'] and val['pk__count'] > 1:
+            clients = Client.objects.filter(client_id=val['client_id'])
+            if clients.count() > 1:
+                main_client = clients.first()
+                the_rest = clients[1:]
+                appointments = Appointment.objects.filter(client__id=the_rest)
+                appointments.update(client=main_client)
+                the_rest.delete()
+
