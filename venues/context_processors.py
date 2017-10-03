aac77809e1728778fd52e6d0bc6d11e22cacf45e
moshthepitt <kjayanoris@gmail.com>
2017-10-04 00:57:25 +0300

    add View object calendar views to main menu

--- venues/context_processors.py
+++ venues/context_processors.py
@@ -1,9 +1,12 @@
-from venues.models import Venue
+from venues.models import Venue, View
 
 
 def venue_processor(request):
+    views = View.objects.filter(customer=request.user.userprofile.customer)
     if request.user.is_authenticated():
-        result = Venue.objects.filter(customer=request.user.userprofile.customer).exclude(is_active=False)
+        result = Venue.objects.filter(
+            customer=request.user.userprofile.customer).exclude(
+            is_active=False)
     else:
         result = []
-    return {'active_venues': result}
+    return {'active_venues': result, 'active_views': views}
