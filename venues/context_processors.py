aeecfd527943e1e5675a7fc61feeeb937cafc2f3
moshthepitt <kjayanoris@gmail.com>
2017-10-04 01:42:59 +0300

    code fix

--- venues/context_processors.py
+++ venues/context_processors.py
@@ -2,11 +2,12 @@ from venues.models import Venue, View
 
 
 def venue_processor(request):
-    views = View.objects.filter(customer=request.user.userprofile.customer)
     if request.user.is_authenticated():
         result = Venue.objects.filter(
             customer=request.user.userprofile.customer).exclude(
             is_active=False)
+        views = View.objects.filter(customer=request.user.userprofile.customer)
     else:
         result = []
+        views = []
     return {'active_venues': result, 'active_views': views}
